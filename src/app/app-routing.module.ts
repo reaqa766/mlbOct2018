fc49b4d33ae69d4f83d2231632ce16b780c95244
rafaelQ <reaqa766@gmail.com>
2018-10-19 17:01:35 -0400

    Cambio comp News

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewsComponent} from '../app/components/news/news.component';
+import { HomeComponent} from '../app/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path:'home',
+  component: HomeComponent},
+
+  { path:'news',
+  component: NewsComponent}
+
+
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
