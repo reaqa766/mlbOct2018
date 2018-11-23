c78c941fd2c159676b1afca5cc82cfe89edd9449
rafaelQ <reaqa766@gmail.com>
2018-11-23 16:23:16 -0400

    Cambios en Componentes de Jugadores y Pitchers

--- src/app/components/pitchers/p-actives/p-actives.component.ts
+++ src/app/components/pitchers/p-actives/p-actives.component.ts
@@ -111,10 +111,9 @@ export class PActivesComponent implements OnInit {
   onSearchChange() {
     if (this.searchText) {
       this.allItems = this.players.filter(player =>
-        player.stats[0].splits[0].team.name.toLowerCase().includes(this.searchText) ||
-        (player.fullName && player.fullName.toLowerCase().includes(this.searchText)) ||
-        (player.nickName && player.nickName.toLowerCase().includes(this.searchText))  ||
-        player.mlbDebutDate.includes(this.searchText));
+        player.fullName.includes(this.searchText) ||
+        (player.fullName.toLowerCase().includes(this.searchText)) ||
+        (player.fullName.toUpperCase().includes(this.searchText)));
         this.setPage(this.pager.currentPage);
         
       } else {
