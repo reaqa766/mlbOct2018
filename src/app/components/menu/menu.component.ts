522df945231837c5141ca883b1bf95507afdc51f
rafaelQ <reaqa766@gmail.com>
2018-11-26 14:55:39 -0400

    Login y logOut funcionales

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AuthService } from '../../services/auth.service';
+
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  public isLogin: boolean;
+  public nombreUsuario: string;
+  public emailUsuario: string;
+  public fotoUsuario: string;
 
-  constructor() { }
+  constructor(
+    public authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe( auth => {
+      if (auth) {
+        this.isLogin = true;
+        this.nombreUsuario = auth.displayName;
+        this.emailUsuario = auth.email;
+        // this.fotoUsuario = auth.photoURL;
+      } else {
+        this.isLogin = false;
+      }
+    });
+    console.log(this.isLogin);
+    
   }
 
+  onClickLogOut() {
+    this.authService.logout();
+    this.isLogin = false;
+
+
+  }
 }
--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService } from '../../../services/auth.service';
+import {Router } from '@angular/router';
+// import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public email: string;
+  public password: string;
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    // public flashMensaje: FlashMessagesService
+  ) { }
 
   ngOnInit() {
   }
 
+  onSubmitLogin() {
+    this.authService.loginEmail(this.email, this.password)
+    .then( (res) => {
+      this.router.navigate(['/news']);
+    }).catch((err) => {
+      this.router.navigate(['/login']);
+    });
+  // onSubmitLogin() {
+  //   this.authService.loginEmail(this.email, this.password)
+  //   .then( (res) => {
+  //     this.flashMensaje.show('Usuario logado correctamente.',
+  //     {cssClass: 'alert-success', timeout: 4000});
+  //     this.router.navigate(['/privado']);
+  //   }).catch((err) => {
+  //     this.flashMensaje.show(err.message,
+  //     {cssClass: 'alert-danger', timeout: 4000});
+  //     this.router.navigate(['/login']);
+  //   });
+  }
 }
