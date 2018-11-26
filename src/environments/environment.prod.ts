7a8b398a03f4fe3e141f0519d6df6aed9d4e152c
rafaelQ <reaqa766@gmail.com>
2018-11-26 09:54:31 -0400

    Conexion con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB62wdM2fojA_NmUmJKgJxl8hZQ15tfu0Q",
+      authDomain: "mlb2019-86c7e.firebaseapp.com",
+      databaseURL: "https://mlb2019-86c7e.firebaseio.com",
+      projectId: "mlb2019-86c7e",
+      storageBucket: "mlb2019-86c7e.appspot.com",
+      messagingSenderId: "185259638222"
+    }
 };
