ccc801c2606c0959c1b7fbf022fa992bf6823392
reaqanvo <reaqanvo@gmail.com>
2018-12-22 13:29:32 -0400

    Correccion de SmartPhones

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,12 +12,12 @@ export class HomeComponent implements OnInit {
   players = [
   {code: 444482, pregunta: '¿Qué posición juega?', respuesta: 'Left Field', activo: true},
   {code: 446653, pregunta: '¿Cuál es su nombre?', respuesta: 'Jose Lobaton', activo: true},
-  {code: 444876, pregunta: '¿Con que apodo se conoce?', respuesta: 'Magic', activo: true},
-  {code: 467092, pregunta: 'En que equipo jugará en 2019?', respuesta: 'NY Mets', activo: true},
+  {code: 444876, pregunta: '¿Cuál es su apodo?', respuesta: 'Magic', activo: true},
+  {code: 467092, pregunta: 'Equipo para el 2019?', respuesta: 'NY Mets', activo: true},
   {code: 471911, pregunta: '¿Cuántos juegos inició?', respuesta: '30', activo: true},
-  {code: 514888, pregunta: '¿En qué fecha estuvo lesionado?', respuesta: '29 de julio de 2018', activo: true},
+  {code: 514888, pregunta: '¿Fecha de  su lesión?', respuesta: '29 de julio de 2018', activo: true},
   {code: 650402, pregunta: '¿Cuántos jonrones bateó?', respuesta: '24', activo: true},
-  {code: 660670, pregunta: '¿Cuáles premios le otorgaron?', respuesta: 'Entre otros Novato del Año, LN', activo: true}
+  {code: 660670, pregunta: '¿Premio en MLB 2018?', respuesta: 'Novato del Año...', activo: true}
   ];
   constructor() { }
 
