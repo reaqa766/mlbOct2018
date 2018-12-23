6e8809957f67cdf6d7c7f14cfd0ec2d6898faf01
reaqanvo <reaqanvo@gmail.com>
2018-12-23 19:17:14 -0400

    Prueba de modificar el landing page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent implements OnInit {
   {code: 444482, pregunta: '¿Qué posición juega?', respuesta: 'Left Field', activo: true},
   {code: 446653, pregunta: '¿Cuál es su nombre?', respuesta: 'Jose Lobaton', activo: true},
   {code: 444876, pregunta: '¿Cuál es su apodo?', respuesta: 'Magic', activo: true},
-  {code: 467092, pregunta: 'Equipo para el 2019?', respuesta: 'NY Mets', activo: true},
+  {code: 467092, pregunta: '¿Equipo para el 2019?', respuesta: 'NY Mets', activo: true},
   {code: 471911, pregunta: '¿Cuántos juegos inició?', respuesta: '30', activo: true},
   {code: 514888, pregunta: '¿Fecha de  su lesión?', respuesta: '29 de julio de 2018', activo: true},
   {code: 650402, pregunta: '¿Cuántos jonrones bateó?', respuesta: '24', activo: true},
