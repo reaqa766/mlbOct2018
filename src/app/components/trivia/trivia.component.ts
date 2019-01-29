a2f904fedbcba141a877dc0ea10efa0a7cea9498
reaqanvo <reaqanvo@gmail.com>
2019-01-29 09:24:56 -0400

    Cambios en trivia

--- src/app/components/trivia/trivia.component.ts
+++ src/app/components/trivia/trivia.component.ts
@@ -11,17 +11,38 @@ export class TriviaComponent implements OnInit {
   // text_11 = true;
   // text_22 = true;
   form_container = true;
-  selection : string = ' ' ;
+  opcion = ' ' ;
+  selection = ' ' ;
   players = [
-  {code: 444482, pregunta: '¿Qué posición juega?', respuesta: 'Left Field', activo: true, option: true, opcion1: 'Short Stop', opcion2:'Catcher', opcion3:'Left Field'},
-  {code: 446653, pregunta: '¿Cuál es su nombre?', respuesta: 'Jose Lobaton', activo: true, option: true, opcion1: 'Gleyber Torres', opcion2:'José Lobatón', opcion3:'Harold Castro' },
-  {code: 444876, pregunta: '¿Cuál es su apodo?', respuesta: 'Magic', activo: true, option: true, opcion1: 'Magic', opcion2:'Sabanero Soy', opcion3:'Esco' },
-  {code: 467092, pregunta: '¿Equipo para el 2019?', respuesta: 'NY Mets', activo: true, option: true, opcion1: 'Tampa', opcion2:'Yankees', opcion3:'Mets' },
-  {code: 514888, pregunta: '¿Fecha de  su lesión?', respuesta: '29 de julio de 2018', activo: true, option: true, opcion1: '5 Abril', opcion2:'29 Julio', opcion3:'3 Octubre' },
-  {code: 506702, pregunta: '¿Cuantos Hits bateó?', respuesta: '47', activo: true, option: true, opcion1: '75', opcion2:'47', opcion3:'53' },
-  {code: 650402, pregunta: '¿Cuántos jonrones bateó?', respuesta: '24', activo: true, option: true, opcion1: '35', opcion2:'20', opcion3:'24' },
-  {code: 660670, pregunta: '¿Premio en MLB 2018?', respuesta: 'Novato del Año...', activo: true, option: true, opcion1: 'MVP', opcion2:'Rookie of the year', opcion3:'Cy Young' }
-  ];
+  {code: 444482, pregunta: '¿Qué posición juega?',
+  respuesta: 'Left Field',
+  activo: true, option: true,
+  opcion1: 'Short Stop', opcion2: 'Catcher', opcion3: 'Left Field'},
+
+  {code: 446653, pregunta: '¿Cuál es su nombre?',
+  respuesta: 'Jose Lobaton', activo: true, option: true,
+  opcion1: 'Gleyber Torres', opcion2: 'José Lobatón', opcion3: 'Harold Castro' },
+
+  {code: 444876, pregunta: '¿Cuál es su apodo?',
+  respuesta: 'Magic', activo: true, option: true,
+  opcion1: 'Magic', opcion2: 'Sabanero Soy', opcion3: 'Esco' },
+
+  {code: 467092, pregunta: '¿Equipo para el 2019?',
+  respuesta: 'NY Mets', activo: true, option: true, opcion1: 'Tampa', opcion2: 'Yankees', opcion3: 'Mets' },
+
+  {code: 514888, pregunta: '¿Fecha de  su lesión?',
+  respuesta: '29 de julio de 2018', activo: true, option: true, opcion1: '5 Abril', opcion2: 'Julio', opcion3: '3 Octubre' },
+
+  {code: 506702, pregunta: '¿Cuantos Hits bateó?',
+  respuesta: '47', activo: true, option: true, opcion1: '75', opcion2: '47', opcion3: '53' },
+
+  {code: 650402, pregunta: '¿Cuántos jonrones bateó?',
+  respuesta: '24', activo: true, option: true, opcion1: '35', opcion2: '20', opcion3: '24' },
+
+  {code: 660670, pregunta: '¿Premio en MLB 2018?',
+  respuesta: 'Novato del Año...', activo: true, option: true, opcion1: 'MVP', opcion2: 'Rookie of the year', opcion3: 'Cy Young' }
+
+];
   constructor() { }
 
   ngOnInit() {
@@ -33,14 +54,15 @@ textChange(i) {
 }
 
 respChange(i) {
+  console.log('opcion', this.opcion);
   this.players[i].option = !this.players[i].option;
-  this.form_container = !this.form_container;
+  // this.form_container = !this.form_container;
   this.selection = this.opcion;
+  console.log('selection', this.selection);
+
+
 
 }
-
-
-
 }
 
 
