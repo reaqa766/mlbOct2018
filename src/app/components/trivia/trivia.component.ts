0361df43b06a482b8c6a5265fed6cf8c3367de23
Aether AMD <AetherAMD@AMD-MINER.local>
2019-01-31 15:54:46 -0400

    Funcionamiento correcto de la trivia sin acumulacion de respuestas

--- src/app/components/trivia/trivia.component.ts
+++ src/app/components/trivia/trivia.component.ts
@@ -11,11 +11,10 @@ export class TriviaComponent implements OnInit {
   // text_11 = true;
   // text_22 = true;s
   answerDef = ' ' ;
-  answer_array: Array<string[]> = [];
-  answer: Array<string[]> =  [];
+  answer: Array<string> =  [];
   // answer = ' ';
-  respuesta_def = true;
-  respuesta_dada = true;
+  respuesta_def: Array<boolean> = [];
+  respuesta_dada: Array<boolean> = [];
   form_container = true;
   opcion = '0' ;
   selection = ' ' ;
@@ -26,7 +25,7 @@ export class TriviaComponent implements OnInit {
   opcion1: 'Short Stop', opcion2: 'Catcher', opcion3: 'Left Field'},
 
   {code: 446653, pregunta: '¿Cuál es su nombre?',
-  respuesta: 'Jose Lobaton', activo: true, option: true,
+  respuesta: 'José Lobatón', activo: true, option: true,
   opcion1: 'Gleyber Torres', opcion2: 'José Lobatón', opcion3: 'Harold Castro' },
 
   {code: 444876, pregunta: '¿Cuál es su apodo?',
@@ -34,10 +33,10 @@ export class TriviaComponent implements OnInit {
   opcion1: 'Magic', opcion2: 'Sabanero Soy', opcion3: 'Esco' },
 
   {code: 467092, pregunta: '¿Equipo para el 2019?',
-  respuesta: 'NY Mets', activo: true, option: true, opcion1: 'Tampa', opcion2: 'Yankees', opcion3: 'Mets' },
+  respuesta: 'Mets', activo: true, option: true, opcion1: 'Tampa', opcion2: 'Yankees', opcion3: 'Mets' },
 
   {code: 514888, pregunta: '¿Fecha de  su lesión?',
-  respuesta: '29 de julio de 2018', activo: true, option: true, opcion1: '5 Abril', opcion2: 'Julio', opcion3: '3 Octubre' },
+  respuesta: '29 de julio de 2018', activo: true, option: true, opcion1: '5 Abril', opcion2: '29 de julio de 2018', opcion3: '3 Octubre' },
 
   {code: 506702, pregunta: '¿Cuantos Hits bateó?',
   respuesta: '47', activo: true, option: true, opcion1: '75', opcion2: '47', opcion3: '53' },
@@ -46,16 +45,16 @@ export class TriviaComponent implements OnInit {
   respuesta: '24', activo: true, option: true, opcion1: '35', opcion2: '20', opcion3: '24' },
 
   {code: 660670, pregunta: '¿Premio en MLB 2018?',
-  respuesta: 'Novato del Año...', activo: true, option: true, opcion1: 'MVP', opcion2: 'Rookie of the year', opcion3: 'Cy Young' }
+  respuesta: 'Rookie of the year', activo: true, option: true, opcion1: 'MVP', opcion2: 'Rookie of the year', opcion3: 'Cy Young' }
 
 ];
   constructor() { }
 
   ngOnInit() {
-    for (var answer_array  of  this.answer) {
-      this.answer_array.push;
-      console.log('answer_OnInit', this.answer_array);
-      
+    for (var answer_array  of  this.players) {
+      this.answer.push(undefined)
+      this.respuesta_dada.push(true);
+      this.respuesta_def.push(true);
     }
 
   }
@@ -65,10 +64,8 @@ textChange(i) {
 }
 
 respDada(i) {
-  this.respuesta_def = true;
-  this.respuesta_dada = true;
-  this.respuesta_def = !this.respuesta_def;
-  this.respuesta_dada = !this.respuesta_dada;
+  this.respuesta_def[i] = !this.respuesta_def;
+  this.respuesta_dada[i] = !this.respuesta_dada;
   console.log('answer', this.answer);
   console.log('respuesta_def', this.respuesta_def);
   console.log('respuesta_dada', this.respuesta_dada);
