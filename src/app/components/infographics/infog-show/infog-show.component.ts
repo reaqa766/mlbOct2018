79772a5cda10315f6257141ee32d0b19cc58d73e
Aether AMD <AetherAMD@AMD-MINER.local>
2019-02-18 14:10:46 -0400

    Agregado de 4 Nvas Infografias

--- src/app/components/infographics/infog-show/infog-show.component.ts
+++ src/app/components/infographics/infog-show/infog-show.component.ts
@@ -11,12 +11,16 @@ export class InfogShowComponent implements OnInit {
     {number:2, url:'../../../../assets/VZLA EN MLB LIGA NACIONAL.jpg'},
     {number:3, url:'../../../../assets/VZLA EN MLB LIGA AMERICANA.jpg'},
     {number:4, url:'../../../../assets/JugadoresGLVZLAporEstados20181.jpg'},
-     {number:5, url:'../../../../assets/INFOSPRINGTNATIONALEST.png'},
+     {number:5, url:'../../../../assets/INFOSPRINGTNATIONALEST.jpg'},
      {number:6, url:'../../../../assets/VZLAEQUIPOSNATIONALCENTRAL.jpg'},
-     {number:7, url:'../../../../assets/VENEZOLANOS MLB NUEVOS EQUIPOS 2019.jpg'},
-     {number:8, url:'../../../../assets/VENEZOLANOS MLB NUEVOS EQUIPOS 2019 2.jpg'},
-     {number:9, url:'../../../../assets/VENEZOLANOS MLB NUEVOS EQUIPOS 2019 3.jpg'},
-     {number:9, url:'../../../../assets/VENEZOLANOS MLB NUEVOS EQUIPOS 2019 4.jpg'}
+     {number:7, url:'../../../../assets/INFO SPRING T NATIONAL WEST.jpg'},
+     {number:8, url:'../../../../assets/INFO SPRING T AMERICAN ESTE.jpg'},
+     {number:9, url:'../../../../assets/INFO SPRING T AMERICAN CENTRAL.jpg'},
+     {number:10, url:'../../../../assets/INFO SPRING T AMERICAN WEST.jpg'},
+     {number:11, url:'../../../../assets/VENEZOLANOS MLB NUEVOS EQUIPOS 2019.jpg'},
+     {number:12, url:'../../../../assets/VENEZOLANOS MLB NUEVOS EQUIPOS 2019 2.jpg'},
+     {number:13, url:'../../../../assets/VENEZOLANOS MLB NUEVOS EQUIPOS 2019 3.jpg'},
+     {number:14, url:'../../../../assets/VENEZOLANOS MLB NUEVOS EQUIPOS 2019 4.jpg'}
   ]
 
   constructor() { }
