c6a280eb2945c48f1b7c56ffb7196998acb1805a
reaqanvo <reaqanvo@gmail.com>
2019-05-05 20:12:35 -0400

    Cambio en el Menu

--- src/app/components/players/daily/daily.component.ts
+++ src/app/components/players/daily/daily.component.ts
@@ -19,7 +19,7 @@ export class DailyComponent implements OnInit {
   selectedGroup: any;
   elarray: any;
   datesN = 10;
-  searchText: string;
+  searchDate: Date;
   playerAuxList = [];
   counter: number;
   n: number;
@@ -82,26 +82,27 @@ getPlayersMap() {
           }
 
         });
-       
+
         this.allItems = this.players;
         this.setPage(1);
         this.isLoading = false;
         }
         index++;
         console.log('gamePlays', this.gamePlays);
-        
+
     });
   }
 
   }
 
-  onSearchChange() {
-    if (this.searchText) {
+  onSearchDate() {
+    if (this.searchDate) {
       this.allItems = this.players.filter(player =>
-        player.stats[0].splits[0].team.name.toLowerCase().includes(this.searchText) ||
-        (player.fullName && player.fullName.toLowerCase().includes(this.searchText)) ||
-        (player.nickName && player.nickName.toLowerCase().includes(this.searchText))  ||
-        player.mlbDebutDate.includes(this.searchText));
+        player.stats[0].splits[player.stats[0].splits.length].date.includes(this.searchDate));
+        // player.stats[0].splits[0].team.name.toLowerCase().includes(this.searchDate) ||
+        // (player.fullName && player.fullName.toLowerCase().includes(this.searchDate)) ||
+        // (player.nickName && player.nickName.toLowerCase().includes(this.searchDate))  ||
+        // player.mlbDebutDate.includes(this.searchDate));
         this.setPage(this.pager.currentPage);
       } else {
           this.allItems = this.players;
