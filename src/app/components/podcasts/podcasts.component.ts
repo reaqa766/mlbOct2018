31c2905634830b84d36f24c4e8ce13f4cf8f4027
Aether AMD <AetherAMD@AMD-MINER.local>
2019-06-03 16:43:51 -0400

    Actuallizaciond e Transacciones

--- src/app/components/podcasts/podcasts.component.ts
+++ src/app/components/podcasts/podcasts.component.ts
@@ -17,23 +17,6 @@ export class PodcastsComponent implements OnInit {
     {number: 3, url: '../../../../assets/EPISODIO3TalkBeisbol2daTemp2019.jpg'},
     {number: 4,url: '../../../../assets/EPISODIO4TalkBeisbol2daTemp2019pqno.jpg'},
     {number: 4,url: '../../../../assets/EPISODIO 5 Talk Beisbol 2da Temp. 2019.jpg'},
-    // {number: 5, url: '../../../../assets/VENEZOLANOS MLB NUEVOS EQUIPOS 2019 2.jpg'},
-    // {number: 6, url: '../../../../assets/VENEZOLANOS MLB NUEVOS EQUIPOS 2019 3.jpg'},
-    // {number: 7, url: '../../../../assets/VENEZOLANOS MLB NUEVOS EQUIPOS 2019 4.jpg'},
-    // {number: 8, url: '../../../../assets/JugadoresGLVZLAporEstados20181.jpg'},
-    // {number: 9, url: '../../../../assets/INFOSPRINGTNATIONALEST.jpg'},
-    // {number: 10, url: '../../../../assets/VZLAEQUIPOSNATIONALCENTRAL.jpg'},
-    // {number: 11, url: '../../../../assets/INFO SPRING T NATIONAL WEST.jpg'},
-    // {number: 12, url: '../../../../assets/INFO SPRING T AMERICAN ESTE.jpg'},
-    // {number: 13, url: '../../../../assets/INFO SPRING T AMERICAN CENTRAL.jpg'},
-    // {number: 14, url: '../../../../assets/INFO SPRING T AMERICAN WEST.jpg'},
-    // {number: 15, url: '../../../../assets/CamisetasRonald2018.jpg'},
-    // {number: 16, url: '../../../../assets/ALINEACION AMERICANA ESTE 2019.jpg'},
-    // {number: 17, url: '../../../../assets/ALINEACION AMERICANA CENTRAL 2019.jpg'},
-    // {number: 18, url: '../../../../assets/ALINEACION AMERICANA OESTE 2019.jpg'},
-    // {number: 19, url: '../../../../assets/ALINEACION NACIONAL ESTE 2019.jpg'},
-    // {number: 20, url: '../../../../assets/ALINEACION NACIONAL CENTRAL 2019.jpg'},
-    // {number: 21, url: '../../../../assets/ALINEACION NACIONAL OESTE 2019.jpg'}
   ];
 
 
