81443944813d2e8712147858c2a24008d77bd295
Aether AMD <AetherAMD@AMD-MINER.local>
2019-06-04 13:12:14 -0400

    Cambios en Novatos

--- src/app/services/allplayers.service.ts
+++ src/app/services/allplayers.service.ts
@@ -13,6 +13,7 @@ import { Observable } from 'rxjs';
 })
 export class AllplayersService {
   playerCode = 596115;
+  rookieCode = 596115;
   // gameCode = 563385;
 
 
@@ -51,97 +52,12 @@ export class AllplayersService {
     642731, 553869, 650382, 650333
   ];
 
-  // menores 2019 545121,658648, 602074, 650671, 514888,
-  // 408234, 433217, 444482, 444489, 445988, 446653,  471865,
-  // 452678, 455139, 462101, 463610, 465041, 467055,
-  // 467092,  485567, 492802, 500208, 500743, 433587,
-  // 500871, 500874, 501303, 503556,   491696, 
-  // 514888, 514916, 514917, 516949, 517369, 520471,
-  // 521692, 527038, 541645, 541650, 542255, 542364,
-  // 542513, 542583, 545121, 546318, 553869, 553882,
-  // 553902, 553988, 553993, 554054, 570560, 571685,
-  // 575929, 578570, 591712, 591720, 591741, 621592,
-  // 593993, 596059, 600524, 600858, 602922,  591693,
-  //  606115, 606299, 614179, 620439, 605612,
-  // 622758, 501593, 623993,
-  // 645305, 650339, 650402, 650700,
-  // 650907, 660564, 660614, 660634, 660636,
-  // 660670, 660844, 661388, 665828, 665945,
-  // 491676, 433587, 433589, 434671, 444468, 448855,
-  // 468504, 471911, 491646, 501593,
-  // 501625, 521655, 527048, 553878, 575070,
-  // 591693, 593163, 593958, 606208, 608566,
-  // 611093, 621237,  622382,
-  // 622608, 622786, 622795,
-  // 630023, 641154,
-  //  656001, 658530,
-  // 666200
+  private rookiesCode = [
+    650671, 642731, 621592, 602074, 650859, 
+    650333, 650382, 622786, 660761, 658648, 
+    
+  ];
 
-
-  // JUGADORES 2018
-  // 650402, 660670, 542583, 514888, 503556,
-  // 542255, 444489, 501303, 462101,
-  // 516472, 606115, 553902, 641319,
-  // 620439, 453923, 593993, 452678,
-  // 408234, 605612, 465041,
-  // 455139, 575929,
-  // 553869, 444876, 500871, 527038,
-  // 520471, 541645, 554054,
-  // 492802, 514917,
-  // 491676, 546318, 506702,
-  // 446653, 553988, 500874,
-  // 400121, 471083,
-  // 553882, 600524, 596059, 542364, 591741,
-  // 467827, 444482, 606299, 542208,
-  // 541650, 521692, 463610,
-  // 517369, 445988, 467092,
-  // 500743,
-  // 602922, 506703, 570560,
-  // 467055,  596143,
-  // 500208, 553993, 491696, 542513,
-  // 624636, 591720, 472528, 591971, 545121,
-  // 622713
-
-  // PITCHERS 2018
-  // private playersCode = [
-  //   621237,
-  //   501593,
-  //   593993,
-  //   471911, 620454,
-  //   468504, 630023, 571035,
-  //   521655,
-  //   491646,
-  //   448855,
-  //   433587, 500872,
-  //   641154, 608566,
-  //   622795, 591672, 622382,
-  //   622161,
-  //   527048, 433589,
-  //   591693,
-  //   593958, 600965, 541652, 444468,
-  //   614179, 434671,
-  //   622608, 462515,
-  //   624133,
-  //   553878
-
-
-  // ];
-
-
-
-  // Url del Api con los datos de cada juego para un Jugador particular
-  // private _url = 'https://statsapi.mlb.com/api/v1/people?personIds=' + this.playerCode +
-  // '&season=2018&hydrate=stats(group=hitting,type=gameLog)';
-  // private _url = 'https://statsapi.mlb.com/api/v1/people?personIds=' + this.playerCode +
-  // '&season=2018&hydrate=stats(group=[hitting,pitching],type=[career,statSplits],sitCodes=a,sportId=12)';
-  // private _url5 = 'https://statsapi.mlb.com/api/v1/people?personIds=' + this.playerCode +
-  // '&season=2018&hydrate=stats(group=[hitting],type=[pecota],sportId=12)';
-  // private _url1 = 'http://statsapi.mlb.com/api/v1/people/' + this.playerCode + '/stats/game/' +
-  // this.gameCode;
-  // tslint:disable-next-line:max-line-length
-  // private _url4 = 'https://statsapi.mlb.com/api/v1/teams/109/roster?hydrate=person(stats(type=season,season=2018),education)&rosterType=Active';
-  // // tslint:disable-next-line:max-line-length
-  // tslint:disable-next-line:max-line-length
   private _url ='https://statsapi.mlb.com/api/v1/people?personIds=' + this.playerCode +  '&season=2019&hydrate=stats(type=season,season=2019,gameType=R)'
 
 
@@ -153,6 +69,9 @@ export class AllplayersService {
 
   // Obtencion de los datos de temporada de un solo jugador
   getPlayeActive(): Observable<StatsDayliPlayer> {
+    return this.http.get<StatsDayliPlayer>(this._url);
+  }
+  getRookies2019(): Observable<StatsDayliPlayer> {
     return this.http.get<StatsDayliPlayer>(this._url);
   }
 
@@ -190,5 +109,23 @@ export class AllplayersService {
     }
     return dataAllPlayers;
   }
+
+
+  getAllRookiesActives(): Observable<StatsDayliPlayer | undefined>[] {
+    // tslint:disable-next-line:prefer-const
+    let dataAllPlayers: Observable<StatsDayliPlayer | undefined>[] = [];
+    // tslint:disable-next-line:prefer-const
+    for (let code of this.rookiesCode) {
+      this._url = this._url.replace(this.rookieCode.toString(), code.toString());
+      // tslint:disable-next-line:prefer-const
+      let dataP = this.getRookies2019();
+      dataAllPlayers.push(dataP);
+      this.rookieCode = code;
+    }
+    return dataAllPlayers;
+  }
+
+
+
 
 }
