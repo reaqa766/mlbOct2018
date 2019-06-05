6c7a18119bf5581a3e34f5d825082948cda0b20e
Aether AMD <AetherAMD@AMD-MINER.local>
2019-06-05 12:25:30 -0400

    Cambios en HOME Area Superior

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -140,7 +140,7 @@ textChange(i) {
      }
    })
    // Se acaba el sort por la estadistica deseada => se retorna un array
-   .slice(0, 7);
+   .slice(0, 6);
    // console.log('filtrado', filtrado);
 
    return filtrado;
@@ -167,7 +167,7 @@ textChange(i) {
      }
    })
    // Se acaba el sort por la estadistica deseada => se retorna un array
-   .slice(0, 7);
+   .slice(0, 6);
    // console.log('filtrado', filtrado);
 
    return filtrado;
