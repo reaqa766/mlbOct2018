b7efa71ae3e2455adc5c834879bce7f2e529dcc1
Aether AMD <AetherAMD@AMD-MINER.local>
2019-07-04 17:21:16 -0400

    creacion de grid en PRUEBAJSONS

--- src/services/pager.service.ts
+++ src/services/pager.service.ts
@@ -1,5 +1,5 @@
 export class PagerService {
-    getPager(allItems: number, currentPage: number = 1, pageSize: number = 7) {
+    getPager(allItems: number, currentPage: number = 1, pageSize: number = 6) {
         // tslint:disable-next-line:prefer-const
         let totalPages = Math.ceil(allItems / pageSize);
 
@@ -53,7 +53,7 @@ export class PagerService {
 
 
 
-    getPager2(allItems: number, currentPage: number = 1, pageSize: number = 7) {
+    getPager2(allItems: number, currentPage: number = 1, pageSize: number = 6) {
         // tslint:disable-next-line:prefer-const
         let totalPages = Math.ceil(allItems / pageSize);
 
