84b12cbbd758b58d3605f8a88da0c2551e25ae48
Aether AMD <AetherAMD@AMD-MINER.local>
2019-07-18 15:48:59 -0400

    Agregado de modulo de lvbpHome

---
+++ src/app/components/lvbp-home/lvbp-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lvbp-home',
+  templateUrl: './lvbp-home.component.html',
+  styleUrls: ['./lvbp-home.component.css']
+})
+export class LvbpHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
