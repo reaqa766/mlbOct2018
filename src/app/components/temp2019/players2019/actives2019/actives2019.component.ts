858a57f593a8d4bab517f5e748ebe60dcc06635a
Aether AMD <AetherAMD@AMD-MINER.local>
2020-03-05 17:22:40 -0400

    Calculo de total Pts FANTASY

--- src/app/components/temp2019/players2019/actives2019/actives2019.component.ts
+++ src/app/components/temp2019/players2019/actives2019/actives2019.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { Players2019Service } from '../../../../services/players2019.service';
+import * as moment from 'moment';
+import { PlayerspointService} from '../../../../playerspoint.service';
 import { take } from 'rxjs/operators';
 import { Players } from '../../../../../interfaces/players';
 
-import { PagerService } from '../../../../../services/index';
+// tslint:disable-next-line:semicolon
+import { PagerService } from '../../../../../services/index'
+
+// import { PlayersService } from '../../../../services/players.service';
+// import { take } from 'rxjs/operators';
+// import { Players } from '../../../../interfaces/players';
+
+// import { PagerService } from '../../../../services/index';
 
 
 @Component({
@@ -17,94 +25,129 @@ export class Actives2019Component implements OnInit {
   groups: any;
   selectedGroup: any;
   elarray: any;
-  // tslint:disable-next-line:no-inferrable-types
-  datesN: number = 10;
+  datesN = 10;
   searchText: string;
   playerAuxList = [];
   counter: number;
   n: number;
   m: number;
-  // tslint:disable-next-line:no-inferrable-types
-  n1: number = 12;
-  // tslint:disable-next-line:no-inferrable-types
-  n10: number = 5;
-
+  n1 = 12;
+  n10 = 5;
+  // dia1 = moment().format('YYYY-MM-DD');
+  // dia1 = new Date(new Date().getFullYear(),new Date().getMonth(), new Date().getDate() - 1);
+  dia = new Date(new Date().getFullYear(),new Date().getMonth(), new Date().getDate() - 1);
+  // dia = new Date(new Date().getFullYear(),new Date().getMonth(), new Date().getDate() - 1);
   public allItems: any[];
-    // pager object
-    pager: any = {};
+  gamePlays: string;
 
-    // paged items
-    pagedItems: any[];
+  // pager object
+  pager: any = {};
+
+  // paged items
+  pagedItems: any;
+  fechaHoy: Date;
+  sumaTotalPts: any;
+  sumaTotal: any;
 
 
-
-
-  // playersList = [
-  //   {name:"Jose Altuve",
-  //   position : "segunda base"},
-
-  //   {name : "Gleyber Torres",
-  //   position : "segunda base"},
-
-  //   {name : "Ronald Acuña Jr.",
-  //   position : "Leftfield"},
-
-  //   {name : "Ender Inciarte",
-  //   position : "Centerfield"}
-
-  // ]
   isLoading: boolean;
+  sumaTotal2: number;
 
-
-
-
-
-  constructor(private playerService: Players2019Service, private pagerService: PagerService) { }
-
+  constructor(private playerService: PlayerspointService, private pagerService: PagerService) { }
 
   ngOnInit() {
+    // console.log('Jugadores', this.players);
+    console.log('dia', this.dia);
     this.isLoading = true;
-    // this.playerService.getPlayerDaily();
     this.getPlayersMap();
   }
 
-  // Convertir el Array de Observables a un Array de Objetos.
-  // Seleccionar los items necesarios del nuevo Array (con todo el contenido del Json) y colocarlos en un nuevo Array
-  getPlayersMap() {
-    // tslint:disable-next-line:prefer-const
-    let InfoObsPlayer = this.playerService.getAllPlayersActives();
-    let index = 0;
-    // tslint:disable-next-line:prefer-const
-    for (let obs of InfoObsPlayer) {
-      obs.pipe(take(1)).subscribe(res => {
-        this.players.push(res);
 
-        if ((InfoObsPlayer.length - 1) === index) {
-          this.players = this.players.map(player => {
-            const newPlayer: Players = {};
-            Object.assign(newPlayer, player.people[0]);
-            return newPlayer;
-          })
-            .sort(({ fullName: a }, { fullName: b }) => {
-              if (a > b) {
-                return 1;
-              } else if (a < b) {
-                return -1;
-              } else if (a === b) {
-                return 0;
+// Convertir el Array de Observables a un Array de Objetos.
+// Seleccionar los items necesarios del nuevo Array (con todo el contenido del Json)
+// y colocarlos en un nuevo Array
+getPlayersMap() {
+  this.players = [];
+  this.allItems = [];
+  this.sumaTotal = 0;
+  this.setPage(1);
+  const InfoObsPlayer = this.playerService.getAllPlayersDaily3();
+  let index = 0;
+  for (let obs of InfoObsPlayer) {
+    obs.pipe(take(1)).subscribe(res => {
+      this.players.push(res);
+
+      if ((InfoObsPlayer.length - 1) === index) {
+
+        this.players = this.players.map(player => {
+          const newPlayer: Players = {};
+          Object.assign(newPlayer, player.people[0]);
+          return newPlayer;
+        });
+        console.log('Original players: ', this.players);
+        // Se filtran los jugadores que no esten activos (no tienen stats ni splits)
+        this.players = this.players.filter(player =>{
+         if(player.stats && player.stats.length !== 0 && player.stats[0].splits && player.stats[0].splits.length !== 0){
+            for(let i = 0; i < player.stats[0].splits.length; i++){
+              if(player.stats[0].splits[i].date === this.dia){
+                player.indexStatDate = i;
+                  for(let z = 0; z <=i; z++)  {              
+                  this.sumaTotal= this.sumaTotal + (player.stats[0].splits[z].stat.hits*1+player.stats[0].splits[z].stat.strikeOuts*-2);
+                  this.sumaTotal2= (player.stats[0].splits[z].stat.hits*1+player.stats[0].splits[z].stat.strikeOuts*-2);
+                  // this.sumaTotalPts = this.sumaTotalPts.push(this.sumaTotal);
+                  console.log('Z: ', z);
+                  console.log('Sumatotal: ', this.sumaTotal2);
+                  console.log('Sumatotal2: ', this.sumaTotal);
+                  }
+                return true;
               }
-            });
-          this.allItems = this.players;
+            }
+            return false;
+          }
+        })
+        // se ordenan por nombre
+        .sort(({fullName: a}, {fullName: b}) => {
+          if (a > b) {
+            return 1;
+          } else if (a < b) {
+            return -1;
+          } else if (a === b) {
+            return 0;
+          }
 
-    // console.log(JSON.stringify(this.players), 'pbajson');
-          this.setPage(1);
-          this.isLoading = false;
+        });
+        // console.log('This players final: ', this.players);
+       
+   
+        
+        
+
+
+
+
+        this.allItems = this.players;
+        this.setPage(1);
+        this.isLoading = false;
+        // console.log('players', this.players);
         }
         index++;
-      });
-    }
+
+    });
+  }
 
   }
+
+  onSearchDate(fecha) {
+    console.log("FECHA", fecha.srcElement.value);
+    // this.dia= fecha.srcElement.value;
+    this.dia= fecha.srcElement.value;
+    this.getPlayersMap();
+}
+//   sumaParcialDiaria(sumatotal) {
+//     console.log("FECHA", fecha.srcElement.value);
+//     this.dia= fecha.srcElement.value;
+//     this.getPlayersMap();
+// }
 
   onSearchChange() {
     if (this.searchText) {
@@ -117,11 +160,20 @@ export class Actives2019Component implements OnInit {
       } else {
           this.allItems = this.players;
           this.setPage(this.pager.currentPage);
+
         }
         return this.allItems;
+
       }
 
+     
+        
+        
+
+
+
   setPage(page: number) {
+
     // get pager object from service
     this.pager = this.pagerService.getPager(this.allItems.length, page);
 
@@ -129,5 +181,19 @@ export class Actives2019Component implements OnInit {
     this.pagedItems = this.allItems.slice(this.pager.startIndex, this.pager.endIndex + 1);
   }
 
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
